d33f795022a34e0e748dd6ea1ca44306ba4c5e16
Hao Nguyen <haonv@paxcreation.com>
2021-08-10 09:11:20 +0700

    Initial Commit

---
+++ AlienReader/AlienReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlienReaderApp.swift
+//  AlienReader
+//
+//  Created by Hao Nguyen on 10/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AlienReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
